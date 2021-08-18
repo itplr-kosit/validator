9417080dd7879a1b45f9533ba4a877fbf013d17a
Andreas Penski (init) <andreas.penski@init.de>
2021-08-18 16:13:12 +0200

    (fix) build image creation

--- .mvn/createBuildImages.sh
+++ .mvn/createBuildImages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAGS=("3-openjdk-16" "3-jdk-11" "3-jdk-11-openj9" "3-jdk-8" "3-jdk-8-openj9", "3-openjdk-15", "3-openjdk-17")
+TAGS=("3-openjdk-16" "3-jdk-11" "3-jdk-11-openj9" "3-jdk-8" "3-jdk-8-openj9" "3-openjdk-15" "3-openjdk-17")
 
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 for i in "${TAGS[@]}"
@@ -8,7 +8,7 @@ do
   echo Creating build image for "$i"
   {
     echo "FROM maven:${i}"
-  }  >> Dockerfile
+  } >>Dockerfile
   docker build -t "$CI_REGISTRY_IMAGE/maven:$i" .
   docker push "$CI_REGISTRY_IMAGE/maven:${i}"
   rm Dockerfile
