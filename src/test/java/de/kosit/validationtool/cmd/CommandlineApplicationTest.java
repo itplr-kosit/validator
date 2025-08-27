7a7712188d8fe9830503ac726850a1bf8f9bb095
Philip Helger <philip@helger.com>
2025-08-27 16:21:12 +0200

    WiP Sysout debugging

--- src/test/java/de/kosit/validationtool/cmd/CommandlineApplicationTest.java
+++ src/test/java/de/kosit/validationtool/cmd/CommandlineApplicationTest.java
@@ -76,11 +76,17 @@ public class CommandlineApplicationTest {
         final String[] args = { "-?" };
         CommandLineApplication.mainProgram(args);
         assertThat(CommandLine.getErrorOutput()).isEmpty();
+        log.warn("Philip1");
         checkForHelp(CommandLine.getOutputLines());
+        log.warn("Philip2");
     }
 
     private static void checkForHelp(final List<String> outputLines) {
         assertThat(outputLines.size()).isPositive();
+        log.warn("!!!!");
+        for (final String s : outputLines)
+            log.warn(s);
+        log.warn("!!!");
         assertThat(outputLines.stream().filter(l -> l.startsWith("Usage: KoSIT Validator"))).hasSize(1);
     }
 
