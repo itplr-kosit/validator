ce6fa54ebad7e386776e23149b181899e22ddbc7
Philip Helger <philip@helger.com>
2026-06-05 12:41:28 +0200

    Removed license headers

--- .mvn/createBuildImages.sh
+++ .mvn/createBuildImages.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
-
-#
-# Copyright 2017-2022  Koordinierungsstelle für IT-Standards (KoSIT)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 TAGS=("3-jdk-8" "3-jdk-8-openj9" "3-eclipse-temurin-11-alpine" "3-jdk-11-openj9" "3-eclipse-temurin-17-alpine" "3-eclipse-temurin-21-alpine" "3-eclipse-temurin-24-alpine")
 
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
