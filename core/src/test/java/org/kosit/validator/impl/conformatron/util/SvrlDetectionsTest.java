990109f7ffc49bf2bafa72cd6a9473cd3ba93db0
Philip Helger <philip@helger.com>
2026-08-21 14:29:47 +0200

    Ctd

--- core/src/test/java/org/kosit/validator/impl/conformatron/util/SvrlDetectionsTest.java
+++ core/src/test/java/org/kosit/validator/impl/conformatron/util/SvrlDetectionsTest.java
@@ -6,7 +6,7 @@ import org.conformatron.api.model.detection.CTDetectionList;
 import org.conformatron.api.model.detection.CTStandardSeverity;
 import org.junit.jupiter.api.Test;
 import org.oclc.purl.dsdl.svrl.FailedAssert;
-import org.oclc.purl.dsdl.svrl.SchematronOutput;
+import org.oclc.purl.dsdl.svrl.SchematronOutputType;
 
 /**
  * Tests the SVRL severity mapping of {@link SvrlDetections}: {@code @role} takes precedence, {@code @flag} is the
@@ -14,17 +14,17 @@ import org.oclc.purl.dsdl.svrl.SchematronOutput;
  */
 public class SvrlDetectionsTest {
 
-    private static SchematronOutput svrlWith(final String id, final String role, final String flag) {
+    private static SchematronOutputType svrlWith(final String id, final String role, final String flag) {
         final FailedAssert failedAssert = new FailedAssert();
         failedAssert.setId(id);
         failedAssert.setRole(role);
-        failedAssert.setFlag(flag);
-        final SchematronOutput svrl = new SchematronOutput();
-        svrl.getActivePatternAndFiredRuleAndFailedAssert().add(failedAssert);
+        failedAssert.getFlag().add(flag);
+        final SchematronOutputType svrl = new SchematronOutputType();
+        svrl.getActivePatternOrActiveGroupAndFiredRule().add(failedAssert);
         return svrl;
     }
 
-    private static CTStandardSeverity severityOf(final SchematronOutput svrl) {
+    private static CTStandardSeverity severityOf(final SchematronOutputType svrl) {
         final CTDetectionList detections = SvrlDetections.toDetections(svrl, "test.xml");
         return (CTStandardSeverity) detections.getAll().get(0).getSeverity();
     }
